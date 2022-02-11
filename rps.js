3c87233a9c0367b431aa5b7cbe8096bffe2f8692
amargh2 <margherioa@gmail.com>
2022-02-11 16:15:59 -0600

    got a working rock paper scissors game with functional UI - no css styling, but all buttons work and results and outcomes are properly incremented and reported

--- rps.js
+++ rps.js
@@ -1,3 +1,17 @@
+
+/* declare consistent variables variables */
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+
+let wins = 0
+let ties = 0
+let losses = 0
+
+/*initialize game function */
+game()
+
+
 function computerPlay () { 
     /*define a list of possible choices and a method by which the computer's choice is randomly made */
     
@@ -6,23 +20,8 @@ function computerPlay () {
     return computerChoice;
 }
 
-function playerChoice() {
-    let choice = prompt('Rock, paper or scissors?').toLowerCase();
-    return choice;
-}
 
-/*function choiceCheck(choice) {
-    if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-        console.log('That\'s not one of the available choices! Pick rock, paper, or scissors!');
-        choice = prompt('Rock, paper, or scissors?');
-    }
-    else {
-        return choice
-    }
-} */
-
-/*take the player selection and computer selection and compare them to see whether it is a win, a tie, or a loss, and return a string */
-
+/*compare computer choice and player choice */
 function playRound(computerSelection, playerSelection) {
     /*outcomes if the player chooses rock */
     
@@ -58,46 +57,104 @@ function playRound(computerSelection, playerSelection) {
         return('It\'s a tie!')
     }
 }
+/*evaluate outcome and increment corresponding variable */
+function evaluateOutcome(outcome) {
+        splitOutcome = outcome.split(' ')
+        winLoseOrTie = splitOutcome[splitOutcome.length -1]
+        
+        if (winLoseOrTie == 'win!') {
+            return wins ++
+            }
+        else if (winLoseOrTie == 'lose!') {
+            return losses ++
+            }
+        else if (winLoseOrTie == 'tie!') {
+            return ties ++
+            }
+}
 
-/*This next function is where the game will repeat itself five times and tally the wins and losses. */
+/*adds ties, wins, and losses, evaluates and reports in document who won, lost, or if it is a tie after five games*/
+function declareWinner() {
+    gamesPlayed = wins+ties+losses
+    winnerDeclaration = document.querySelector('.declarewinner')
+    if (gamesPlayed == 5) {
+        if (wins > losses && wins > ties) {
+            winnerDeclaration.textContent =('You\'re a winner baby.')
+            wins = 0
+            ties = 0
+            losses = 0
+        }
+        else if (losses > wins && losses > ties) {
+            winnerDeclaration.textContent =('Sorry! Out of 5 games, you lost!')
+            wins = 0
+            ties = 0
+            losses = 0
+        }
+        else {
+            winnerDeclaration.textContent =('It\'s a tie! Play again.')
+            wins = 0
+            ties = 0
+            losses = 0
+        }
+        
+    }
+}
+/*clears the declareWinner text after player has played 5 games and plays again */
+function clearDeclareWinner() {
+    winnerDeclaration = document.querySelector('.declarewinner')
+    winnerDeclaration.textContent = ''
+}
+/*counts the wins and losses and adds them to the document in a separate div*/
+function tally(){
+    count = document.querySelector('.tally')
+    count.textContent = (`wins: ${wins}; losses: ${losses}; ties:${ties}`)
+}
 
 
 function game() {
-    /*declare variables for wins, ties, and losses that will be incremented as they accumulate in the for loop */    
-    let wins=0
-    let ties = 0
-    let losses = 0
-
-    /*this loop gets the player's choice, the computer's choice, compares them, and reports the outcome to the player. then the outcome is tallied
-    and the corresponding variable is incremented */
-    for (let gameTries = 0; gameTries < 5; gameTries++) {
-        let playerSelection = playerChoice();
+    /*defines variables, and button and eventlisteners; paper triggers papergame, etc. probably can be refactored*/
+    resultReport = document.querySelector('.results')
+    rock.addEventListener('click', rockGame);
+    paper.addEventListener('click', paperGame);
+    scissors.addEventListener('click', scissorsGame)
+    
+    
+/*if player clicks rock*/    
+    function rockGame() {
+        let playerSelection = 'rock';
         let computerSelection = computerPlay();
         outcome = playRound(computerSelection, playerSelection);
-        console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}. ${outcome}.`);
-        if (outcome == "You win!") {
-            wins += 1;
-        }
-        else if (outcome == "It's a tie!") {
-            ties += 1;
-        }
-        else if (outcome == "You lose!") {
-            losses += 1;
-        }
+        resultReport = document.querySelector('.results')
+        resultReport.textContent =(`You chose ${playerSelection} and the computer chose ${computerSelection}. ${outcome}`);
+        evaluateOutcome(outcome)
+        tally()
+        clearDeclareWinner() 
+        declareWinner()
+    }
+/*if player clicks paper*/
+    function paperGame() {
+        let playerSelection = 'paper';
+        let computerSelection = computerPlay();
+        outcome = playRound(computerSelection, playerSelection);
+        resultReport = document.querySelector('.results')
+        resultReport.textContent = (`You chose ${playerSelection} and the computer chose ${computerSelection}. ${outcome}`);
+        evaluateOutcome(outcome)
+        tally()
+        clearDeclareWinner() 
+        declareWinner()
+    };
+/*if player clicks scissors*/
+    function scissorsGame() {
+        let playerSelection = 'scissors';
+        let computerSelection =computerPlay();
+        outcome = playRound(computerSelection, playerSelection);
+        resultReport = document.querySelector('.results')
+        resultReport.textContent =(`You chose ${playerSelection} and the computer chose ${computerSelection}. ${outcome}`)
+        evaluateOutcome(outcome)
+        tally()
+        clearDeclareWinner() 
+        declareWinner()
     }
     
-    /*report the wins and losses and declare a winner */
+};
 
-    console.log(`Out of 5 games, you won ${wins} games, lost ${losses}, and tied ${ties}.`)
-    if (wins > losses) {
-        console.log('You won more games than the computer! Congrats!')
-    }
-    else if (losses > wins) {
-        console.log('The computer won more games than you, but don\'t let it get you down.')
-    }
-    else if (wins == losses) {
-        console.log('It\'s a tie! Better play again!')
-    }
-}
-
-game()
